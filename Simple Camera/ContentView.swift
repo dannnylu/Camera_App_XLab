fd0a7890da994177001b03814ddf8659c6b9ca73
dannnylu <dannylu160@gmail.com>
2022-07-08 14:23:48 -0500

    Initial Commit

---
+++ Simple Camera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simple Camera
+//
+//  Created by Danny Lu on 7/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple Camera/Simple_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CameraApp.swift
+//  Simple Camera
+//
+//  Created by Danny Lu on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
