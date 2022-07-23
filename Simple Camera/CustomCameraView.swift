7785565d081480f9ed5749f6b1ae2473b249b5c1
dannnylu <dannylu160@gmail.com>
2022-07-23 00:46:58 -0500

    I'm not too certain on what the android Ui looks like so CustomCameraView is just a placeholder. Also this needs to be tested first thing next week I'm not in the lab so I haven't been able to test the code on an actual iPhone. -DL

---
+++ Simple Camera/CustomCameraView.swift
@@ -0,0 +1,46 @@
+//
+//  CustomCameraView.swift
+//  Simple Camera
+//
+//  Created by Danny Lu on 7/23/22.
+//
+
+import SwiftUI
+
+struct CustomCameraView: View {
+    
+    let cameraService = CameraSevice()
+    @Binding var capturedImage: UIImage?
+    
+    @Environment(\.presentationMode) private var presentationMode
+    
+    var body: some View {
+        ZStack {
+            CameraView(cameraService: cameraService) { result in
+                switch result {
+                case .success(let photo):
+                    if let data = photo.fileDataRepresentation() {
+                        capturedImage = UIImage(data: data)
+                        presentationMode.wrappedValue.dismiss()
+                    } else {
+                        print("Error: no image data found")
+                    }
+                case .failure(let err):
+                    print(err.localizedDescription)
+                }
+            }
+            
+            VStack {
+                Spacer()
+                Button(action: {
+                    cameraService.capturePhoto()
+                }, label: {
+                    Image(systemName: "circle")
+                        .font(.system(size: 72))
+                        .foregroundColor(.white)
+                })
+                .padding(.bottom)
+            }
+        }
+    }
+}
